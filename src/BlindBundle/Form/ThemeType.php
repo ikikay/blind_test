9103b5d59a6719b82cc3871354da6081aab2ef9a
ikika <ikika@HADAR>
2018-05-03 18:10:07 +0200

    CRUD Theme (Route en YML)

---
+++ src/BlindBundle/Form/ThemeType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace BlindBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ThemeType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('libelle');
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'BlindBundle\Entity\Theme'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'blindbundle_theme';
+    }
+
+
+}
